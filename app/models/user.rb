f54d6d7f6e7ffa81797f6c3f3f4b921062d61966
Anthony Catullo <acatullo4@gmail.com>
2021-05-04 18:45:17 -0500

    add google auth broken

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
     has_secure_password
     validates :email, presence: true, uniqueness: true
+
+
+    def self.from_omniauth(auth)
+        # Creates a new user only if it doesn't exist
+        where(email: auth.info.email).first_or_initialize do |user|
+          user.name = auth.info.name
+          user.email = auth.info.email
+        end
+      end
 end
