f22f2c2a90f9fde116717dd22b5c1efc0128236b
Anthony Catullo <acatullo4@gmail.com>
2021-05-04 20:56:54 -0500

    removed google oauth keys

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV['61847186403-dvcc6a8nfitajfq73oja8o8d4jp682vf.apps.googleusercontent.com'], ENV['T6EfrfP8AsgMaEQBbYLGDJzw']
-  end
