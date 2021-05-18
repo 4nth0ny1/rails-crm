689a368c2f9850542722b7f1492a8d96adbb4add
Anthony Catullo <acatullo4@gmail.com>
2021-05-17 21:14:45 -0500

    add opp crud but does not create opps yet

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -1,10 +1,65 @@
 class OpportunitiesController < ApplicationController
+    before_action :set_opportunity, only: %i[ show edit update destroy ]
+
     def index
+        @opportunities = Opportunity.all
     end 
 
-    def show 
+    def new
+        @opportunity = Opportunity.new
     end
 
-    def new 
+    def show 
     end 
+
+    def edit 
+    end
+
+    def create
+  
+          @opportunity = current_user.opportunities.build(account_params)
+      
+          respond_to do |format|
+            if @opportunity.save
+              format.html { redirect_to @opportunity, notice: "Opportunity was successfully created." }
+              format.json { render :show, status: :created, location: @opportunity }
+            else
+              format.html { render :new, status: :unprocessable_entity }
+              format.json { render json: @opportunity.errors, status: :unprocessable_entity }
+            end
+          end
+        
+      end
+
+      def update
+        respond_to do |format|
+          if @opportunity.update(opportunity_params)
+            format.html { redirect_to @opportunity, notice: "Opportunity was successfully updated." }
+            format.json { render :show, status: :ok, location: @opportunity }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+            format.json { render json: @opportunity.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+      def destroy
+        @opportunity.destroy
+        respond_to do |format|
+          format.html { redirect_to opportunities_url, notice: "Opportunity was successfully destroyed." }
+          format.json { head :no_content }
+        end
+      end
+
+
+      private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_opportunity
+          @opportunity = Opportunity.find(params[:id])
+      end
+  
+      # Only allow a list of trusted parameters through.
+      def opportunity_params
+        params.require(:opportunity).permit(:products, :contacts)
+      end
 end 
