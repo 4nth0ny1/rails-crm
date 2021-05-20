1771c1ba86cc99021848aa7d098bb095a80ad7ff
Anthony Catullo <acatullo4@gmail.com>
2021-05-20 11:18:56 -0500

    add time to connections but doesnt work

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -3,4 +3,9 @@ class Connection < ApplicationRecord
     belongs_to :account
     belongs_to :contact, required: false
     belongs_to :opportunity, required: false
+
+
+    def show_date 
+        DateTime.now
+    end 
 end
