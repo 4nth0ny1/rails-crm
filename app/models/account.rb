f1749209f02e989186721e0d86f3f9bbfb462c0a
Anthony Catullo <acatullo4@gmail.com>
2021-05-23 15:44:14 -0500

    add new accounts to dashboard

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,4 +4,7 @@ class Account < ApplicationRecord
     has_many :connections
     belongs_to :user
     has_many :tasks, through: :contacts
+
+    scope :weekly_accounts, -> { where(created_at: Time.current.beginning_of_week..Time.current) }
+
 end
