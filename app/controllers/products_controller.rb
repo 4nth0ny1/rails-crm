eac2f45f21e16319e31870e99fa63f52199a92c3
Anthony Catullo <acatullo4@gmail.com>
2021-05-24 20:08:19 -0500

    refactor products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,38 +16,25 @@ class ProductsController < ApplicationController
       end
   
       def create
-    
             @product = current_user.products.build(product_params)
-        
-            respond_to do |format|
               if @product.save
-                format.html { redirect_to @product, notice: "Product was successfully created." }
-                format.json { render :show, status: :created, location: @product }
+                redirect_to @product, notice: "Product was successfully created." 
               else
-                format.html { render :new, status: :unprocessable_entity }
-                format.json { render json: @product.errors, status: :unprocessable_entity }
+                render :new, status: :unprocessable_entity 
               end
-            end
         end
   
         def update
-          respond_to do |format|
             if @product.update(product_params)
-              format.html { redirect_to @product, notice: "Product was successfully updated." }
-              format.json { render :show, status: :ok, location: @product }
+              redirect_to @product, notice: "Product was successfully updated." 
             else
-              format.html { render :edit, status: :unprocessable_entity }
-              format.json { render json: @product.errors, status: :unprocessable_entity }
+              render :edit, status: :unprocessable_entity 
             end
-          end
         end
   
         def destroy
           @product.destroy
-          respond_to do |format|
-            format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
-            format.json { head :no_content }
-          end
+          redirect_to products_url, notice: "Product was successfully destroyed." 
         end
   
   
