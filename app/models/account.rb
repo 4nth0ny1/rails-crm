3fad9b7c5cfcc6cf901d925dd84816bcb9c6a02b
Anthony Catullo <acatullo4@gmail.com>
2021-05-25 20:39:23 -0500

    add validaitons to account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,6 +4,9 @@ class Account < ApplicationRecord
     has_many :connections
     belongs_to :user
     has_many :tasks, through: :contacts
+    validates :company_name, presence: true, length: {minimum: 2}
+    validates :address, presence: true, length: {minimum: 2}
+    validates :phone, presence: true, length: {minimum: 7}
 
     scope :weekly_accounts, -> { where(created_at: Time.current.beginning_of_week..Time.current) }
 
