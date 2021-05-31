cfbeff3c1b67cdf237439e372c15dd3de95585a7
Anthony Catullo <acatullo4@gmail.com>
2021-05-30 19:36:55 -0500

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'about', to: 'about#index', as: 'about'
 
 end
 
